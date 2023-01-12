a0401a235f4aa113d1b45fb69fe120bb7bb7db27
Tim Bryant <timcalvin@gmail.com>
2023-01-12 14:46:28 -0500

    Initial Commit

---
+++ MyLibrary/MyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryApp.swift
+//  MyLibrary
+//
+//  Created by Timothy Bryant on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
